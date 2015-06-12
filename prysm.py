f24816383f598d7b242a47651b43a563e5a75503
Adway Dhillon <adhillon6@gatech.edu>
2015-06-12 11:07:45 +0530

    Removed the hardcoded header row

--- prysm.py
+++ prysm.py
@@ -5,20 +5,26 @@ import HTML
 def readFile(fileName):
     with open(fileName) as csvfile:
         reader = csv.reader(csvfile)
+        i = 0
         table_data = []
         for row in reader:
-            table_data.append(reader.next())
-        gen_html(table_data)
+            if (i == 0):
+                header_row = row
+            else:
+                table_data.append(row)
+            i += 1
+        gen_html(header_row, table_data)
 
-def gen_html(table_data):
+def gen_html(head_row, table_data):
     HTMLFILE = 'prysm_output.html'
     f = open(HTMLFILE, 'w')
-    html_table = HTML.Table(table_data, 
-        header_row = ['Test Name', 'Test Status', 'Test Start Time', 'Test End Time', 'Test Run Time'])
+    html_table = HTML.Table(table_data,
+            header_row = head_row)
     html_table = str(html_table)
     f.write(html_table)
     f.write('<p>')
     f.close()
+
 
 def main():
     parser = argparse.ArgumentParser(description='Read the file')
