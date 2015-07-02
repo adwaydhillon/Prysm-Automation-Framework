163d225d4fa175c83b3924dc701096057122d01c
Adway Dhillon <adhillon6@gatech.edu>
2015-07-02 13:02:27 +0530

    Validating the project

---
+++ scripts/vaildate_proj.py
@@ -0,0 +1,11 @@
+
+
+def main():
+	parser = argparse.ArgumentParser(description='Validate the project')
+    parser.add_argument('proj_path', help='Input the path of the project to be validated')
+    args = parser.parse_args()
+    proj_path = args.proj_path
+    validate(proj_path)
+
+if __name__ == "__main__":
+	main()
