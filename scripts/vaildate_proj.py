3eb10e0ae19b7c7d6582eb477362cdd8d93b5092
Adway Dhillon <adhillon6@gatech.edu>
2015-07-02 13:08:31 +0530

    walking through os.dir

--- scripts/vaildate_proj.py
+++ scripts/vaildate_proj.py
@@ -1,11 +1,16 @@
+import argparse
+import os
 
+def validate(proj_path):
+	for i in os.listdir(os.getcwd()):
+		print i
 
 def main():
 	parser = argparse.ArgumentParser(description='Validate the project')
-    parser.add_argument('proj_path', help='Input the path of the project to be validated')
-    args = parser.parse_args()
-    proj_path = args.proj_path
-    validate(proj_path)
+   	parser.add_argument('proj_path', help='Input the path of the project to be validated')
+   	args = parser.parse_args()
+   	proj_path = args.proj_path
+   	validate(proj_path)
 
 if __name__ == "__main__":
 	main()
