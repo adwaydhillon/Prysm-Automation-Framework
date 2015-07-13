a3ccbc12ca53b01b1e3cd706cae5d6c1cad478d1
Adway Dhillon <adhillon6@gatech.edu>
2015-07-13 12:27:23 +0530

    changes in the python scraping script

--- scripts/scrape_config.py
+++ scripts/scrape_config.py
@@ -4,7 +4,7 @@
 #author          :Adway Dhillon
 #date            :20150713
 #version         :1.0.1
-#usage           :python scrape_config.py [folder_path]
+#usage           :python scrape_config.py [YAML_file_path]
 #notes           :
 #python_version  :Python 2.7.6
 #==============================================================================
@@ -13,14 +13,21 @@ import argparse
 
 def main():
 	parser = argparse.ArgumentParser(description='Scrape the YAML config files')
-	parser.add_argument('simConfig_path', help='Input the path of the Simulation Environment Configuration')
-	parser.add_argument('testConfig_path', help='Input the path of the Test Run Configuration')
+	parser.add_argument('file_path', help='Input the path of the Simulation Environment Configuration')
 	args = parser.parse_args()
-	simConfig_path = args.simConfig_path
-	args = parser.parse_args()
-	testConfig_path = args.testConfig_path
+	file_path = args.file_path
+	scrape_file(file_path)
 
-def scrape_simConfig(simConfig_path):
+def scrape_file(file_path):
+	details = {}
+	
+	with open(file_path) as f:
+		content = f.readlines()
+	f.close()
+
+	for line in content:
+		if line[0] != '#':
+			print line
 	
 
 if __name__ == "__main__":
