61941bcbaa55dbdfdfc7ba218fc888e86d329594
Adway Dhillon <adhillon6@gatech.edu>
2015-07-21 15:22:11 +0530

    running the python scripts

--- app/validated.cpp
+++ app/validated.cpp
@@ -13,3 +13,12 @@ Validated::~Validated()
     delete ui;
 }
 
+//NEED TO LOOK INTO THIS
+void Validated::on_pushButton_2_clicked()
+{
+    QProcess *run = new QProcess(this);
+    run->setProcessChannelMode(QProcess::ForwardedChannels);
+    run->start("python", QStringList() << "/Users/adwaydhillon/Documents/Development/Prysm_Automation_Framework/scripts/reportGen.py"
+                    << proj_path);
+    QByteArray output = run->readAllStandardOutput();
+}
